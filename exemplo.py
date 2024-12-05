cc6e9efa172846e625c306e0960e602bab182edf
Marcos Coutinho <veniciusm4@gmail.com>
2024-12-05 10:00:27 -0300

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,12 @@
+
+numero = int(input("Insira um número: "))
+
+if numero > 0:
+        print(f"O numero {numero} é positivo")
+elif numero ==0:
+       print(f"O numero é zero")
+else:
+        print(f"o numero {numero} é negativo")    
+
+print("Fim do programa")                   
+        
